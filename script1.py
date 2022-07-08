ddf3eaaf6de393a5c51bcb74397ccb7775033784
griceldavaldez <gricel.va99@gmail.com>
2022-07-08 18:36:48 +0000

    initial commit

---
+++ script1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print("Imprime los numeros del 1 al 10")
+for i in range(0,10):
+	print("Nº " + str(i+1))
---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Tablero:
+# 0 = Espacio vacío
+# 1 = Moneda
+# 2 = Enemigo
+# 3 = Meta
+tablero = [[0, 0, 1],
+           [0, 2, 0],
+           [1, 0, 3]]
+
+print("Tamaño del tablero " + str(len(tablero)))
+print("Elementos del tablero " + str(tablero))
